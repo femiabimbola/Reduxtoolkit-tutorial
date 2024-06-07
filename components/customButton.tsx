d6522bfc127b93babb00f65d292fb2cf1b84ce71
Femi Abimbola <femiabimbola6@gmail.com>
2024-06-07 16:53:17 +0100

    using the custom

--- components/customButton.tsx
+++ components/customButton.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useSelector, useDispatch } from "react-redux";
+import { useAppSelector, useAppDispatch } from "../redux/hooks";
 import { decrement, increment } from "../redux/features/counterSlice";
 
 export function Counter() {
-  const count = useSelector((state: any) => state.counter.value);
-  const dispatch = useDispatch();
+  // const count = useSelector((state: any) => state.counter.value);
+  // const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
+  const count = useAppSelector((state) => state.counter.value);
 
   return (
     <div>
